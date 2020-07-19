9dffa64b5ff064e9faa6faaa706538e810445797
Cem Ezgin <cemezgn@gmail.com>
2020-07-19 19:02:35 +0300

    Entity tests finish

--- src/test/java/com/app/entity/discount/CampaignTest.java
+++ src/test/java/com/app/entity/discount/CampaignTest.java
@@ -1,4 +1,35 @@
 package com.app.entity.discount;
 
+import com.app.entity.Category;
+import com.app.service.discount.calculate.type.DiscountType;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CampaignTest {
+    Category testCategory = new Category("TestCategory");
+    Campaign testCampaign = new Campaign(testCategory,10,3, DiscountType.Amount);
+    Campaign testCampaign2 = new Campaign(testCategory,5,20, DiscountType.Rate);
+
+    @Test
+    public void test_getDiscountShouldBeGiven()
+    {
+        assertEquals(10, testCampaign.getDiscount());
+        assertEquals(5, testCampaign2.getDiscount());
+    }
+
+    @Test
+    public void test_getMinimumItemShouldBeGiven()
+    {
+        assertEquals(3, testCampaign.getMinimumItem());
+        assertEquals(20, testCampaign2.getMinimumItem());
+    }
+
+    @Test
+    public void test_getDiscountTypeShouldBeGiven()
+    {
+        assertEquals(DiscountType.Rate, testCampaign2.getDiscountType());
+        assertEquals(DiscountType.Amount, testCampaign.getDiscountType());
+    }
+
 }
--- src/test/java/com/app/entity/discount/CouponTest.java
+++ src/test/java/com/app/entity/discount/CouponTest.java
@@ -1,4 +1,33 @@
 package com.app.entity.discount;
 
+import com.app.service.discount.calculate.type.DiscountType;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CouponTest {
+    Coupon testCoupon = new Coupon(10,3.6, DiscountType.Amount);
+    Coupon testCoupon2 = new Coupon(5,22.5, DiscountType.Rate);
+
+    @Test
+    public void test_getDiscountShouldBeGiven()
+    {
+        assertEquals(3.6, testCoupon.getDiscount());
+        assertEquals(22.5, testCoupon2.getDiscount());
+    }
+
+    @Test
+    public void test_getMinimumAmountShouldBeGiven()
+    {
+        assertEquals(10, testCoupon.getMinimumAmount());
+        assertEquals(5, testCoupon2.getMinimumAmount());
+    }
+
+    @Test
+    public void test_getDiscountTypeShouldBeGiven()
+    {
+        assertEquals(DiscountType.Amount, testCoupon.getDiscountType());
+        assertEquals(DiscountType.Rate, testCoupon2.getDiscountType());
+    }
+
 }
