76520336514144e54a6e2b9da097189c24710743
Cem Ezgin <cemezgn@gmail.com>
2020-07-19 16:23:26 +0300

    Add cartItem and Category tests

--- src/test/java/com/app/entity/CartItemTest.java
+++ src/test/java/com/app/entity/CartItemTest.java
@@ -1,4 +1,29 @@
 package com.app.entity;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CartItemTest {
+    private Category testCategory1 = new Category("TestCategory1");
+    private Product testProduct1 = new Product("TestProduct1", 9999.99, testCategory1);
+    private CartItem cartItem = new CartItem(testProduct1, 10);
+
+    @Test
+    public void test_GetProduct_Should_ReturnGivenProduct() {
+        assertEquals(testProduct1, cartItem.getProduct());
+    }
+
+    @Test
+    public void test_GetQuantity_Should_ReturnGivenQuantity() {
+        assertEquals(10, cartItem.getQuantity());
+    }
+
+    @Test
+    public void test_SetQuantity_Should_ReturnEditedQuantity() {
+        cartItem.setQuantity(5);
+
+        assertEquals(5, cartItem.getQuantity());
+    }
 }
--- src/test/java/com/app/entity/CategoryTest.java
+++ src/test/java/com/app/entity/CategoryTest.java
@@ -1,4 +1,25 @@
 package com.app.entity;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 public class CategoryTest {
+    Category testParentCategory = new Category("TestParentCategory");
+    Category testChildCategory = new Category("TestChildCategory", testParentCategory);
+
+    @Test
+    public void test_getTitleShouldReturnSameWithGiven() {
+        assertEquals("TestParentCategory", testParentCategory.getTitle());
+        assertEquals("TestChildCategory", testChildCategory.getTitle());
+    }
+
+    @Test
+    public void test_parentCategory() {
+        assertEquals(testChildCategory.getParentCategory(), testParentCategory);
+        assertNull(testParentCategory.getParentCategory());
+    }
+
+
 }
